9bf887ff4414158fcb089479d3eaa4a1b4f24c89
Darryl Johnson <dandycodes@gmail.com>
2022-09-07 21:06:12 -0400

    profile picture added ready to be added to resume 9/6

--- src/editable-stuff/config.js
+++ src/editable-stuff/config.js
@@ -39,7 +39,7 @@ const mainBody = {
 const about = {
   show: true,
   heading: "About Me",
-  imageLink: require("./.DSC_3451.pxd.icloud"),
+  imageLink: require("../../src/assets/img/techPicture.jpeg"),
   imageSize: 375,
   message:
     "My name is Darryl Johnson, I go by DJ or Deej; I love sports, technology, food, and head-bands. I am a current educator and motivator residing in the state of Florida. When not working you can find me working out, reading a book, trying foods from different cultures, or watching TV. My goal is to transtion from education into a software developer role in which I can develop my skills and grow with a great company. ",
@@ -67,12 +67,12 @@ const leadership = {
     "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Vitae auctor eu augue ut lectus arcu bibendum at varius. Libero justo laoreet sit amet cursus sit amet. Imperdiet dui accumsan sit amet nulla facilisi morbi. At auctor urna nunc id. Iaculis urna id volutpat lacus laoreet non curabitur gravida. Et magnis dis parturient montes nascetur ridiculus mus mauris. In nisl nisi scelerisque eu ultrices vitae auctor. Mattis nunc sed blandit libero volutpat sed cras ornare. Pulvinar neque laoreet suspendisse interdum consectetur libero.",
   images: [
     { 
-      img: require("../editable-stuff/hashirshoaeb.png"), 
+      img: require("../../src/assets/img/techPicture.jpeg"), 
       label: "First slide label", 
       paragraph: "Nulla vitae elit libero, a pharetra augue mollis interdum." 
     },
     { 
-      img: require("../editable-stuff/hashirshoaeb.png"), 
+      img: require("../../src/assets/img/techPicture.jpeg"), 
       label: "Second slide label", 
       paragraph: "Nulla vitae elit libero, a pharetra augue mollis interdum." 
     },
@@ -126,12 +126,10 @@ const experiences = {
   data: [
     {
       role: 'Lead Teacher & Department Chair',// Here Add Company Name
-      companylogo: require('../assets/img/dell.png'),
       date: 'Jan 2020 – Present',
     },
     {
       role: 'Teacher',
-      companylogo: require('../assets/img/boeing.png'),
       date: 'Aug 2018 – January 2020',
     },
   ]
